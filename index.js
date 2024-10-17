1228f01d750f0537061adb892233232a676e3624
Abdirabi440 <osmanabdirabi440@gmail.com>
2024-10-17 04:13:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name); 
+}
+destructivelyAppendCat(Zoya)
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name); 
+}
+destructivelyPrependCat(Maya)
+
+
+function destructivelyRemoveLastCat() {
+  cats.pop(); 
+}
+
+
+function destructivelyRemoveFirstCat() {
+  cats.shift(); 
+}
+
+function appendCat(name) {
+  return [...cats, name]; // Correctly using 'name' parameter, no undeclared 'Zoya'
+}
+appendCat(Lara)
+
+
+function prependCat(name) {
+  return [name, ...cats]; 
+}
+prependCat(Jimmy)
+
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1); 
+}
